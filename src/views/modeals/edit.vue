643fb5f665fae4784bfd7e0c92fc134bf328d162
kangzhicheng <kangzhicheng@gk.com>
2022-03-11 10:35:59 +0800

    1

---
+++ src/views/modeals/edit.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <el-dialog
+  title="提示"
+  v-model="dialogVisible"
+  width="30%">
+  <!-- <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span> -->
+</el-dialog>
+
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    name:'edit',
+    
+  setup(){
+  
+   const dialogVisible=ref(false)
+//    dialogVisible.value = inject('emit')
+    const show = ()=>{
+         dialogVisible.value=true
+        
+    }
+   return{ 
+       dialogVisible,
+       show
+    }
+  }
+ 
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/vue3.vue
@@ -0,0 +1,94 @@
+<template>
+<el-card>
+<div>
+        <el-button style="margin:20px 0" type="primary" @click="adds">新增奥利给</el-button>
+</div>
+<el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+      <!-- 操作 -->
+      <el-table-column
+        label="操作">
+      </el-table-column>
+    </el-table>
+    <!-- 新增 -->
+    <edit ref="mapday"/>
+
+
+</el-card>
+</template>
+
+<script>
+import { onMounted, getCurrentInstance, ref, } from '@vue/runtime-core'
+import edit from './modeals/edit.vue'
+export default {
+   name:'vue3',
+   components:{
+       edit
+   },
+   setup(){
+        const tableData=ref([])
+        let dialogVisible=ref(false)
+        let mapday =ref()
+
+    onMounted(()=>{
+
+    })
+     const adds= ()=>{
+       mapday.value.show()
+
+     }
+   
+
+          //  输出
+       return {
+         tableData,
+         dialogVisible,
+         mapday,
+         adds
+       }
+
+   }
+  
+
+
+}
+</script>
+
+<style scoped > 
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 480px;
+  }
+
+</style>
