d733209669bab6146fa9ca490fec3a53cbc9e898
kangzhicheng <kangzhicheng@gk.com>
2022-03-19 15:18:20 +0800

    1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
             <i v-if="!collapse" class="el-icon-s-fold"></i>
             <i v-else class="el-icon-s-unfold"></i>
         </div>
-        <div class="logo">一坨奥利给</div>
+        <div class="logo">91.鹏先生巅峰之作</div>
         <div class="header-right">
             <div class="header-user-con">
                 <!-- 消息中心 -->
